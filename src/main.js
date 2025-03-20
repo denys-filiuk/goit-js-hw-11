218b08644b815afb33a3581df556a8ef43f216a4
denys-filiuk <filiuk_dp@ukr.net>
2025-03-20 23:41:12 +0200

    added gallery cleanup and error handling

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import { fetchSearchResult } from './js/pixabay-api.js';
 import { renderSearchCard } from './js/render-functions.js';
 
 const form = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', async event => {
   event.preventDefault();
-  const searchQuery = event.target.elements.search.value.trim();
 
+  const searchQuery = event.target.elements.search.value.trim();
   if (!searchQuery) {
     iziToast.show({
       message: 'Введіть слово для пошуку!',
@@ -16,9 +17,12 @@ form.addEventListener('submit', event => {
     });
     return;
   }
+  gallery.innerHTML = '';
 
-  fetchSearchResult(searchQuery).then(images => {
-    if (images.length === 0) {
+  try {
+    const images = await fetchSearchResult(searchQuery);
+
+    if (!images || images.length === 0) {
       iziToast.show({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
@@ -30,7 +34,16 @@ form.addEventListener('submit', event => {
     } else {
       renderSearchCard(images);
     }
-  });
+  } catch (error) {
+    console.error('Error while retrieving images:', error);
+    iziToast.show({
+      message: 'An error occurred while searching. Please try again!',
+      position: 'topRight',
+      backgroundColor: '#EF4040',
+      messageColor: '#FAFAFB',
+      iconColor: '#FAFAFB',
+    });
+  }
 
   form.reset();
 });
