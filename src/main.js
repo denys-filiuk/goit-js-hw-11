a2173646d8ea7174ec720bdacdf8154b95d4196e
denys-filiuk <filiuk_dp@ukr.net>
2025-03-23 18:46:03 +0200

    edits

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { fetchSearchResult } from './js/pixabay-api.js';
 import { renderSearchCard } from './js/render-functions.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
